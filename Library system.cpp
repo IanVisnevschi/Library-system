c7c2f9afd1654ee799fd33f9ff24f6bfed2ccb86
IanVisnevschi <22407525@bucks.ac.uk>
2026-02-19 13:23:49 +0000

    Member class addition
    member class finished and tested in main

--- Library system.cpp
+++ Library system.cpp
@@ -38,3 +38,49 @@ public:
 };
 
 
+class Member : public User {
+    private:
+       int borrowedBooks;
+
+    public:
+        Member(int userId, string userName, string userEmail)
+            : User(userId, userName, userEmail) {
+            borrowedBooks = 0;
+       }
+
+        void borrowBook() {
+            borrowedBooks++;
+            cout<<getName()<<"borrowed a book. Total borrowed: " << borrowedBooks << endl;
+
+       }
+       
+        void returnBook() {
+            if (borrowedBooks > 0) {
+                borrowedBooks --;
+                cout<<getName()<<"returned a book. Total borrowed: " <<borrowedBooks << endl;
+            }
+            else {
+                cout <<getName()<<"has no books to return. \n"<<endl;
+            }
+
+       }
+
+        void displayInfo() override {
+            User::displayInfo();
+            cout<<"Borrowed Books: "<<borrowedBooks<<endl;
+        }
+};
+
+
+int main() {
+    Member m1(67,"Ian","Ian@gmail.com");
+
+    m1.displayInfo();
+    m1.borrowBook();
+    m1.borrowBook();
+    m1.returnBook();
+    m1.displayInfo();
+
+    return 0;
+}
+
